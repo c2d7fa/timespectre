47bae0e2fd575c5eeb4e6e363e77f4b4f3af61c0
Jonas Hvid <mail@johv.dk>
2020-04-10 18:55:08 +0200

    Implement placeholder for server API

--- server/lib/timespectre/plug.ex
+++ server/lib/timespectre/plug.ex
@@ -1,10 +1,23 @@
 defmodule Timespectre.Plug do
-  use Plug.Builder
+  use Plug.Router
 
+  plug :match
   plug Plug.Static, at: "/", from: "../dist/"
-  plug :not_found
+  plug :dispatch
 
-  def not_found(conn, _opts) do
+  get "/api/counter" do
+    conn |> send_resp(200, "0")
+  end
+
+  post "/api/counter/increment" do
+    conn |> send_resp(501, "TODO")
+  end
+
+  post "/api/counter/decrement" do
+    conn |> send_resp(501, "TODO")
+  end
+
+  match _ do
     conn |> send_resp(404, "Not found")
   end
 end
