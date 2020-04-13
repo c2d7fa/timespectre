057ff6e5617eb30feb33bd5b67f4a4ccbfc2c1ba
Jonas Hvid <mail@johv.dk>
2020-04-13 20:41:07 +0200

    Store counter value on server

--- server/lib/timespectre/plug.ex
+++ server/lib/timespectre/plug.ex
@@ -3,24 +3,35 @@ defmodule Timespectre.Plug do
 
   plug :match
   plug Plug.Static, at: "/", from: "../dist/"
-  plug :dispatch
+
+  # We want to store the counter as state. We can use Agents to store state. We
+  # need to make the agent's PID available to each route. We can do this by
+  # passing it in as an option. Apparently, passing in builder_opts() here let's
+  # us set the options in the init function.
+  plug :dispatch, builder_opts()
+
+  def init(_opts) do
+    {:ok, pid} = Agent.start_link(fn () -> 0 end)
+    pid
+  end
 
   get "/api/counter" do
-    conn |> send_resp(200, "2")
+    counter = Agent.get(opts, fn x -> x end)
+    send_resp(conn, 200, to_string(counter))
   end
 
   post "/api/counter/increment" do
-    IO.puts("Increment")
-    conn |> send_resp(501, "TODO")
+    Agent.update(opts, fn x -> x + 1 end)
+    send_resp(conn, 200, "")
   end
 
   post "/api/counter/decrement" do
-    IO.puts("Decrement")
-    conn |> send_resp(501, "TODO")
+    Agent.update(opts, fn x -> x - 1 end)
+    send_resp(conn, 200, "")
   end
 
   match _ do
-    conn |> send_resp(404, "Not found")
+    send_resp(conn, 404, "Not found")
   end
 end
 
