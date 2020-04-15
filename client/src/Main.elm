00fc1bf70eac58a565fecb18f21476c51be7c231
Jonas Hvid <mail@johv.dk>
2020-04-15 22:13:00 +0200

    Add skeleton of basic UI to app

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,9 +1,27 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, span, text)
-import Html.Events exposing (onClick)
+import Html
+import Html.Attributes as Attr
+import Html.Events as Ev
 import Http
+import Time
+
+
+type alias ActiveSession =
+    Maybe Time.Posix
+
+
+type alias Session =
+    { start : Time.Posix
+    , end : Time.Posix
+    }
+
+
+type alias Model =
+    { sessions : List Session
+    , active : ActiveSession
+    }
 
 
 main =
@@ -15,21 +33,21 @@ main =
         }
 
 
-type Model
-    = Value Int
-    | Loading
-
-
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( Loading, Http.get { url = "/api/counter", expect = Http.expectString Received } )
+    ( { sessions =
+            [ { start = Time.millisToPosix 1586977440000, end = Time.millisToPosix 1586980560000 }
+            , { start = Time.millisToPosix 1586997440000, end = Time.millisToPosix 1586999560000 }
+            ]
+      , active = Nothing
+      }
+    , Cmd.none
+    )
 
 
 type Msg
-    = Increment
-    | Decrement
-    | Received (Result Http.Error String)
-    | Posted (Result Http.Error ())
+    = StartSession
+    | EndSession
 
 
 subscriptions : Model -> Sub Msg
@@ -38,41 +56,108 @@ subscriptions model =
 
 
 update msg model =
-    case ( msg, model ) of
-        ( Received result, _ ) ->
-            case result of
-                Ok s ->
-                    case String.toInt s of
-                        Just n ->
-                            ( Value n, Cmd.none )
+    ( case msg of
+        StartSession ->
+            model
 
-                        Nothing ->
-                            ( Loading, Cmd.none )
-
-                _ ->
-                    ( Loading, Cmd.none )
-
-        ( _, Loading ) ->
-            ( Loading, Cmd.none )
-
-        ( Increment, Value n ) ->
-            ( Value (n + 1), Http.post { url = "/api/counter/increment", body = Http.emptyBody, expect = Http.expectWhatever Posted } )
-
-        ( Decrement, Value n ) ->
-            ( Value (n - 1), Http.post { url = "/api/counter/decrement", body = Http.emptyBody, expect = Http.expectWhatever Posted } )
-
-        ( Posted _, _ ) ->
-            ( model, Cmd.none )
+        EndSession ->
+            model
+    , Cmd.none
+    )
 
 
+view : Model -> Html.Html Msg
 view model =
-    case model of
-        Loading ->
-            text "Loading..."
+    Html.div [] [ viewActive model.active, viewControls model.active, viewSessions model.sessions ]
 
-        Value n ->
-            div []
-                [ text (String.fromInt n)
-                , button [ onClick Increment ] [ text "Increment" ]
-                , button [ onClick Decrement ] [ text "Decrement" ]
-                ]
+
+viewActive : ActiveSession -> Html.Html Msg
+viewActive active =
+    case active of
+        Nothing ->
+            Html.text "No active session."
+
+        Just start ->
+            Html.text ("Active session since " ++ formatTime start)
+
+
+viewControls : ActiveSession -> Html.Html Msg
+viewControls active =
+    case active of
+        Nothing ->
+            Html.button [] [ Html.text "Start" ]
+
+        Just _ ->
+            Html.button [] [ Html.text "End" ]
+
+
+viewSessions : List Session -> Html.Html Msg
+viewSessions sessions =
+    Html.div []
+        [ Html.h1 [] [ Html.text "Sessions" ]
+        , Html.div [] (List.map viewSession sessions)
+        ]
+
+
+viewSession : Session -> Html.Html Msg
+viewSession session =
+    Html.div [ Attr.class "session" ]
+        [ session.start |> formatTime |> Html.text
+        , Html.text " to "
+        , session.end |> formatTime |> Html.text
+        ]
+
+
+formatTime : Time.Posix -> String
+formatTime time =
+    (time |> Time.toYear Time.utc |> String.fromInt)
+        ++ "-"
+        ++ (time |> Time.toMonth Time.utc |> formatMonth)
+        ++ "-"
+        ++ (time |> Time.toDay Time.utc |> String.fromInt)
+        ++ " "
+        ++ (time |> Time.toHour Time.utc |> String.fromInt |> String.padLeft 2 '0')
+        ++ ":"
+        ++ (time |> Time.toMinute Time.utc |> String.fromInt |> String.padLeft 2 '0')
+        ++ ":"
+        ++ (time |> Time.toSecond Time.utc |> String.fromInt |> String.padLeft 2 '0')
+
+
+formatMonth : Time.Month -> String
+formatMonth m =
+    case m of
+        Time.Jan ->
+            "Jan"
+
+        Time.Feb ->
+            "Feb"
+
+        Time.Mar ->
+            "Mar"
+
+        Time.Apr ->
+            "Apr"
+
+        Time.May ->
+            "May"
+
+        Time.Jun ->
+            "Jun"
+
+        Time.Jul ->
+            "Jul"
+
+        Time.Aug ->
+            "Aug"
+
+        Time.Sep ->
+            "Sep"
+
+        Time.Oct ->
+            "Oct"
+
+        Time.Nov ->
+            "Nov"
+
+        Time.Dec ->
+            "Dec"
