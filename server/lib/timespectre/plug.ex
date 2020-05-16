50bb8aaea9427aed37d75c69a106e9fa5cf19440
Jonas Hvid <mail@johv.dk>
2020-05-16 21:23:35 +0200

    Serve 'index.html' on '/'

--- server/lib/timespectre/plug.ex
+++ server/lib/timespectre/plug.ex
@@ -8,6 +8,10 @@ defmodule Timespectre.Plug do
 
   forward "/api", to: Timespectre.ApiPlug
 
+  get "/" do
+    send_file(conn, 200, "../dist/index.html")
+  end
+
   match _ do
     send_resp(conn, 404, "Not found")
   end
