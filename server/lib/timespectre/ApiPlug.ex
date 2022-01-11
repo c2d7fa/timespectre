e5499bca604fd0c2c1d922c45654c4a3d79d0436
Jonas Hvid <mail@johv.dk>
2022-01-11 19:44:08 +0100

    Move macro for getting authenticated user to `AuthenticationPlug`

--- server/lib/timespectre/ApiPlug.ex
+++ server/lib/timespectre/ApiPlug.ex
@@ -3,6 +3,9 @@ defmodule Timespectre.ApiPlug do
 
   use Plug.Router
 
+  require Timespectre.AuthenticationPlug
+  import Timespectre.AuthenticationPlug, only: [{:conn_user, 0}]
+
   plug Timespectre.AuthenticationPlug
   plug :match
   plug Plug.Parsers, parsers: [:json], pass: [], json_decoder: Jason
@@ -10,33 +13,27 @@ defmodule Timespectre.ApiPlug do
 
   forward "/stats", to: Timespectre.StatsPlug
 
-  defmacrop authenticated_user do
-    quote do
-      var!(conn).assigns[:authenticated_user]
-    end
-  end
-
   put "/sessions/:id" do
     start_time = conn.body_params["start"]
     end_time = conn.body_params["end"]
-    query! "INSERT INTO sessions (user, id, start, end) VALUES (?1, ?2, ?3, ?4)", bind: [authenticated_user(), id, start_time, end_time]
+    query! "INSERT INTO sessions (user, id, start, end) VALUES (?1, ?2, ?3, ?4)", bind: [conn_user(), id, start_time, end_time]
     send_resp(conn, 200, "")
   end
 
   put "/sessions/:id/end" do
     end_time = conn.body_params["_json"]
-     query! "UPDATE sessions SET end = ?3 WHERE user = ?1 AND id = ?2", bind: [authenticated_user(), id, end_time]
+     query! "UPDATE sessions SET end = ?3 WHERE user = ?1 AND id = ?2", bind: [conn_user(), id, end_time]
     send_resp(conn, 200, "")
   end
 
   put "/sessions/:id/notes" do
     notes = conn.body_params["_json"]
-    query! "UPDATE sessions SET notes = ?3 WHERE user = ?1 AND id = ?2", bind: [authenticated_user(), id, notes]
+    query! "UPDATE sessions SET notes = ?3 WHERE user = ?1 AND id = ?2", bind: [conn_user(), id, notes]
     send_resp(conn, 200, "")
   end
 
   delete "/sessions/:id" do
-    query! "UPDATE sessions SET deleted = 1 WHERE user = ?1 AND id = ?2", bind: [authenticated_user(), id]
+    query! "UPDATE sessions SET deleted = 1 WHERE user = ?1 AND id = ?2", bind: [conn_user(), id]
     send_resp(conn, 200, "")
   end
 
@@ -51,7 +48,7 @@ defmodule Timespectre.ApiPlug do
       GROUP BY id
       ORDER BY end IS NOT NULL, end DESC, start DESC
       LIMIT 50
-      """, bind: [authenticated_user()]
+      """, bind: [conn_user()]
 
     split_comma_list = fn s ->
       String.split(s, ",") |> Enum.filter(fn x -> x != "" end)
@@ -77,7 +74,7 @@ defmodule Timespectre.ApiPlug do
         NOT deleted AND
         end IS NULL
       GROUP BY sessions.id
-    """, bind: [authenticated_user()]
+    """, bind: [conn_user()]
 
     split_comma_list = fn s ->
       String.split(s, ",") |> Enum.filter(fn x -> x != "" end)
@@ -112,13 +109,13 @@ defmodule Timespectre.ApiPlug do
   # Rename or create tag
   post "/sessions/:id/tags/:tag" do
     new_tag = conn.body_params["_json"]
-    query! ~s{DELETE FROM session_tags WHERE user = ?1 AND session_id = ?2 AND tag = ?3}, bind: [authenticated_user(), id, tag]
-    query! ~s{INSERT OR IGNORE INTO session_tags (user, session_id, tag) VALUES (?1, ?2, ?3)}, bind: [authenticated_user(), id, new_tag]
+    query! ~s{DELETE FROM session_tags WHERE user = ?1 AND session_id = ?2 AND tag = ?3}, bind: [conn_user(), id, tag]
+    query! ~s{INSERT OR IGNORE INTO session_tags (user, session_id, tag) VALUES (?1, ?2, ?3)}, bind: [conn_user(), id, new_tag]
     send_resp(conn, 200, "")
   end
 
   delete "/sessions/:id/tags/:tag" do
-    query! ~s{DELETE FROM session_tags WHERE user = ?1 AND session_id = ?2 AND tag = ?3}, bind: [authenticated_user(), id, tag]
+    query! ~s{DELETE FROM session_tags WHERE user = ?1 AND session_id = ?2 AND tag = ?3}, bind: [conn_user(), id, tag]
     send_resp(conn, 200, "")
   end
 
--- server/lib/timespectre/AuthenticationPlug.ex
+++ server/lib/timespectre/AuthenticationPlug.ex
@@ -2,6 +2,16 @@ defmodule Timespectre.AuthenticationPlug do
   import Plug.Conn
   alias Timespectre.Authentication, as: Auth
 
+  def authenticated_user(conn) do
+    conn.assigns[:authenticated_user]
+  end
+
+  defmacro conn_user do
+    quote do
+      Timespectre.AuthenticationPlug.authenticated_user(var!(conn))
+    end
+  end
+
   def init(_opts), do: nil
 
   def call(conn, _opts) do
